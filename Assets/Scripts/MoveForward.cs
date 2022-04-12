38d9895529927dd540ed9e9b6b0ae3f7da58a5c0
Kai-6 <89489271+Kai-6@users.noreply.github.com>
2022-04-11 19:14:14 -0700

    tutorial 1 and 2 done

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+  public float speed = 20.0f;
+  public Vector3 rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rotation = new Vector3(Random.Range(-10,10), Random.Range(-10, 10), Random.Range(-10, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
+        transform.Rotate(rotation, Space.World);
+    }
+}
