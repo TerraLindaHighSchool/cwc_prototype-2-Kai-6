2fa99d7c1d70f14ac53027c8dfd7f0819bb23fd8
Kai-6 <89489271+Kai-6@users.noreply.github.com>
2022-04-14 15:37:45 -0700

    Adding High Score system with file editing because I'm bored

---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour
+{
+    private FileStream dataFileStream;
+    private string dataFileName;
+    public Text scoreBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreBox = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void WriteToScore()
+    {
+        using (dataFileStream = new FileStream(dataFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+        using (StreamWriter sw = new StreamWriter(dataFileStream))
+            sw.WriteLine("Score, Name, Date");
+    }
+}
